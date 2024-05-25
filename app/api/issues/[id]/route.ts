c0b517558d2e8ac513a809f47a9c5c9417c0196c
Mohammed Usama <usamabhakrani@gmail.com>
2024-05-25 17:53:27 +0500

    patch schema

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -11,7 +11,6 @@ interface ParamsProps {
 }
 
 export const GET = async (request: NextRequest, { params }: ParamsProps) => {
-
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -24,15 +23,24 @@ export const GET = async (request: NextRequest, { params }: ParamsProps) => {
 };
 
 export const PATCH = async (request: NextRequest, { params }: ParamsProps) => {
-  
   const session = await getServerSession(authOptions);
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = createIssueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 404 });
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  if (body.assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: body.assignedToUserId },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid User" }, { status: 400 });
+    }
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
@@ -43,7 +51,11 @@ export const PATCH = async (request: NextRequest, { params }: ParamsProps) => {
 
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: { title: body.title, description: body.description },
+    data: {
+      title: body.title,
+      description: body.description,
+      assignedToUserId: body.assignedToUserId,
+    },
   });
   return NextResponse.json(updatedIssue, { status: 201 });
 };
